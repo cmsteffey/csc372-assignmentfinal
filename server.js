776baeaabafa49290ccc64bddc6756e972e6ec3f
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-18 18:00:10 -0500

    Added static and macro files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-﻿let app = require('express')();
+﻿const express = require("express");
+let app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/templates");
+app.use(express.static("static"));
 app.get('/', (req, res) => {
 
     res.render('index', {welcomeText: "Beep!"});
