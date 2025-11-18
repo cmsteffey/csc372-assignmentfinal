ca89dc14fc4a744aeb3a52f4f72b58711a346361
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-18 17:50:02 -0500

    Added starter ejs and express server

---
+++ server.js
@@ -0,0 +1,8 @@
+﻿let app = require('express')();
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/templates");
+app.get('/', (req, res) => {
+
+    res.render('index', {welcomeText: "Beep!"});
+})
+app.listen(5656)
