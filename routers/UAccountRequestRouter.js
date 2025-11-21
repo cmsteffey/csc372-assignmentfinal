891ffe359e127fcfd7c4f227a8d145238ba086dc
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-21 01:56:15 -0500

    Update account router to use barrier from middleware rather than default import

--- routers/UAccountRequestRouter.js
+++ routers/UAccountRequestRouter.js
@@ -1,6 +1,6 @@
 ﻿import {Router} from "express";
 import accountController from "../controllers/UAccountController.js";
-import authenticationBarrier from "../middleware/AuthenticationBarrier.js";
+import {authenticationBarrier} from "../middleware/Authentication.js";
 const router = Router();
 router.get('/admin/user-accounts', accountController.allUserAccounts);
 router.get('/me', authenticationBarrier, accountController.me)
