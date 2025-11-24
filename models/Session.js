bb25f327923b4277ab98ffa6f3d794e4e1bcf5ec
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-24 15:24:18 -0500

    Comment out deleteSession

--- models/Session.js
+++ models/Session.js
@@ -1,4 +1,4 @@
-﻿import pool from './Pool.js'
+import pool from './Pool.js'
 import crypto from "node:crypto"
 async function getAccountWithSessionUuid(uuid){
     let queryResult = await pool.query("SELECT user_account.* from session join user_account on user_account.id = session.user_id where key=$1::uuid", [uuid.toString()]);
@@ -10,7 +10,7 @@ async function makeSession(accountId){
     await pool.query("INSERT INTO session (user_id, key) VALUES ($1, $2::uuid)", [accountId, key.toString()]);
     return key;
 }
-async function deleteSession(uuid){
+/*async function deleteSession(uuid){
     await pool.query("DELETE FROM session where key=$1::uuid", [uuid]);
-}
+}*/
 export default {makeSession, getAccountWithSessionUuid}
