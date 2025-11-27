3cd8d72236f9c6e8056ba83dbb1df5713d0579af
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-26 19:29:07 -0500

    Organize journal entry portions by their account category

--- models/JournalEntry.js
+++ models/JournalEntry.js
@@ -2,7 +2,7 @@ import pool from "./Pool.js"
 import fAccountType from "./FAccountType.js";
 async function getTransactionPortionsForUser(userId){
     return (await pool.query(
-        "SELECT journal_entry.*, financial_account.nickname as faccount_nickname, financial_account.category as faccount_category, transaction_portion.amount as amount, transaction_portion.description as description from journal_entry join transaction_portion on transaction_portion.journal_entry = journal_entry.id join financial_account on transaction_portion.financial_account = financial_account.id where financial_account.owner = $1 order by journal_entry.for_date NULLS FIRST, transaction_portion.journal_entry", [userId])).rows.map(x=>({...x, faccount_category_string: fAccountType[x.faccount_category].name}));
+        "SELECT journal_entry.*, financial_account.nickname as faccount_nickname, financial_account.category as faccount_category, transaction_portion.amount as amount, transaction_portion.description as description from journal_entry join transaction_portion on transaction_portion.journal_entry = journal_entry.id join financial_account on transaction_portion.financial_account = financial_account.id where financial_account.owner = $1 order by journal_entry.for_date NULLS FIRST, transaction_portion.journal_entry, financial_account.category, financial_account.nickname", [userId])).rows.map(x=>({...x, faccount_category_string: fAccountType[x.faccount_category].name}));
 }
 async function createJournalEntry(name, date){
     return (await pool.query(
