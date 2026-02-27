e1845267b17bb0a74fdc1210e54f065483022985
Charles Steffey <65865777+cmsteffey@users.noreply.github.com>
2026-02-27 16:29:46 -0500

    Add cb_account_id to addCreditCardInfo function

--- models/CreditCardInfo.js
+++ models/CreditCardInfo.js
@@ -1,6 +1,6 @@
 ﻿import pool from "./Pool.js";
 
-async function addCreditCardInfo(account_id, cashback){
-    return (await pool.query('INSERT INTO credit_card_info (financial_account, default_cashback_pct) VALUES ($1, $2)', [account_id, cashback])).rows;
+async function addCreditCardInfo(account_id, cashback, cb_account_id){
+    return (await pool.query('INSERT INTO credit_card_info (financial_account, default_cashback_pct, default_cashback_account) VALUES ($1, $2, $3)', [account_id, cashback, cb_account_id])).rows;
 }
 export default {addCreditCardInfo};
