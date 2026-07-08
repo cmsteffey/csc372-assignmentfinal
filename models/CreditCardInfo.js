77dd2cc6458608845d24d633d0e57a5ccae9323d
Charles Steffey <65865777+cmsteffey@users.noreply.github.com>
2026-07-08 19:18:27 -0400

    Add conflict resolution

--- models/CreditCardInfo.js
+++ models/CreditCardInfo.js
@@ -1,6 +1,6 @@
-﻿import pool from "./Pool.js";
+import pool from "./Pool.js";
 
 async function addCreditCardInfo(account_id, cashback, cb_account_id){
-    return (await pool.query('INSERT INTO credit_card_info (financial_account, default_cashback_pct, default_cashback_account) VALUES ($1, $2, $3)', [account_id, cashback, cb_account_id])).rows;
+    return (await pool.query('INSERT INTO credit_card_info (financial_account, default_cashback_pct, default_cashback_account) VALUES ($1, $2, $3) ON CONFLICT (financial_account) DO UPDATE', [account_id, cashback, cb_account_id])).rows;
 }
 export default {addCreditCardInfo};
